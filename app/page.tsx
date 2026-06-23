e8920aac9a0297787e1b84941fce3ee421e0e0c1
uthiramuthu29 <uthiramuthusp29@gmail.com>
2026-06-23 17:16:45 +0530

    Under Dev

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>F1 Dashboard</h1>
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="text-5xl font-bold ">F1 Dashboard</h1>
         <h2>Latest results and race calendar</h2>
         <p>Under development...</p>
       </main>
